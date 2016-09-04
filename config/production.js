f99acaf010be913c5ded22f6696d624c28fd3523
Andrew Uldin <uldin@yandex-team.ru>
2016-09-04 23:45:18 +0300

    Production configs

---
+++ config/production.js
@@ -0,0 +1,8 @@
+module.exports = {
+    server: {
+        port: 80
+    },
+    mongodb: {
+        host: 'mongodb://heroku_bqsvpc6q:27mro3g9jcfgrb7qslcrsv3v1s@ds021026.mlab.com:21026/heroku_bqsvpc6q'
+    }
+};
